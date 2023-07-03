e78c98a55f7f8b04b8cec4c14a7a687b4a9eb71f
Here East <nastiapiven@gmail.com>
2023-07-03 21:51:08 +0200

    fix: fix strings and numbers

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -278,9 +278,9 @@ function isString(value) {
 
 
 /**
- * Returns playid card id.
+ * Returns playing card id.
  *
- * Playing cards inittial deck inclides the cards in the following order:
+ * Playing cards initial deck inclides the cards in the following order:
  *
  *  'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
  *  'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -233,8 +233,7 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-  if (Number(value)) return value;
-  return def;
+  return Number(value) ? value : def;
 }
 
 module.exports = {
